a509a28ae1340912795dd60e53dad41016263c3f
liminghao <liminghao@people.cn>
2022-07-05 16:09:59 +0800

    first commitment

--- hk_science.js
+++ hk_science.js
@@ -22,7 +22,7 @@
 
   var record = { title: window.location.href };
   // read json
-  if (localStorage.getItem("record") === null) {
+  if (localStorage.getItem("record") == null) {
     // save json
     var json = JSON.stringify(record);
     // save to local storage
@@ -47,7 +47,7 @@
         var json = JSON.stringify(curr_record);
         var blob = new Blob([json]);
         var href = URL.createObjectURL(blob);
-        download(href, "record.json", "text/json");
+        download(href, "HKM_record.json", "text/json");
         URL.revokeObjectURL(href);
       } catch (e) {
         console.log(e);
@@ -87,7 +87,39 @@
         body.removeChild(document.getElementById("timestamp"));
       }
     }
+    if (e.key === "p") {
+      var localStorage_tmp = window.localStorage;
+      var record_tmp = JSON.parse(localStorage_tmp.getItem("record"));
+      console.log(record_tmp);
+    }
   });
+
+  // add mouse scroll listener
+  document.addEventListener("scroll", function () {
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+      console.log("reach the bottom");
+      var scroll = {
+        title: window.location.href,
+        time: (Date.now() / 1000) | 0,
+        event: "pageend",
+      };
+      modifyRecords("scrolls",scroll);
+    }
+  });
+
+  // add onready listener
+  document.addEventListener("readystatechange", function(){
+    var scroll = {
+      title: window.location.href,
+      time: (Date.now() / 1000) | 0,
+      event: "pagestart",
+    };
+    modifyRecords("scrolls",scroll);
+  })
 
   async function createVideoRecorder() {
     let stream = await navigator.mediaDevices.getDisplayMedia({
@@ -96,22 +128,24 @@
     });
     // recording in mp4 format
 
-    let mime = MediaRecorder.isTypeSupported('video/webm; codecs=vp9') ? 'video/webm; codecs=vp9' : 'video/webm';
+    let mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
+      ? "video/webm; codecs=vp9"
+      : "video/webm";
     let mediaRecorder = new MediaRecorder(stream, {
-      mimeType: mime
-    })
+      mimeType: mime,
+    });
     let recordedChunks = [];
-    mediaRecorder.addEventListener('dataavailable', event => {
+    mediaRecorder.addEventListener("dataavailable", (event) => {
       recordedChunks.push(event.data);
-    })
-    mediaRecorder.addEventListener('stop', () => {
+    });
+    mediaRecorder.addEventListener("stop", () => {
       let blob = new Blob(recordedChunks, {
         type: mime,
       });
       let href = URL.createObjectURL(blob);
       download(href, "record.webm");
       URL.revokeObjectURL(href);
-    })
+    });
     mediaRecorder.start();
   }
 
@@ -145,12 +179,12 @@
     document.getElementsByTagName("head")[0].appendChild(style);
 
     // set recording styles
-    recording.style.position = "absolute";
+    recording.style.position = "fixed";
     recording.style.top = "6px";
-    recording.style.left = "120px";
+    recording.style.left = "140px";
     recording.style.width = "12px";
     recording.style.height = "12px";
-    recording.style.backgroundColor = "#ff00007d";
+    recording.style.backgroundColor = "#ff0000bf";
     recording.style.borderRadius = "50%";
     recording.style.zIndex = "9999";
     recording.style.animation = "recording 1s infinite";
@@ -164,17 +198,17 @@
     var time_elem = document.createElement("time");
 
     // set time_elem styles
-    time_elem.style.position = "absolute";
+    time_elem.style.position = "fixed";
     time_elem.style.top = "0";
     time_elem.style.left = "0";
-    time_elem.style.width = "110px";
+    time_elem.style.width = "130px";
     time_elem.style.padding = "0 2px 0 2px";
     time_elem.style.color = "white";
     time_elem.style.backgroundColor = "#1c1d1e";
     time_elem.style.opacity = "80%";
     time_elem.style.fontSize = "large";
     time_elem.style.fontWeight = "bold";
-    time_elem.style.fontFamily = "'Noto Sans TC'";
+    time_elem.style.fontFamily = "system-ui";
     time_elem.style.zIndex = "9999";
     time_elem.style.textAlign = "center";
 
@@ -187,45 +221,23 @@
     }, 100);
   }
 
-  // add listeners to all elements
-  function addListeners() {
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].onclick = function () {
-        // print href
-        if (this.href) {
-          try {
-            // template for step
-            const step = {
-              title: this.innerText,
-              url: this.href,
-              type: "body",
-              time: (Date.now() / 1000) | 0,
-            };
-            // read local storage
-            var localStorage_tmp = window.localStorage;
-            var record_tmp = JSON.parse(localStorage_tmp.getItem("record"));
-            console.log(this.href);
-            console.log((Date.now() / 1000) | 0);
-            console.log(record_tmp);
-            // check if header
-            checkIfHeader("header", this, step);
-            // add step
-            // if no key steps, create one
-            if (!record_tmp.hasOwnProperty("steps")) {
-              record_tmp.steps = [step];
-            } else {
-              record_tmp.steps.push(step);
-            }
-            // save json
-            var json = JSON.stringify(record_tmp);
-            // save to local storage
-            localStorage_tmp.setItem("record", json);
-          } catch (e) {
-            console.log(e);
-          }
-        }
-      };
+  // modify records
+  function modifyRecords(key_name, key) {
+    // read local storage
+    var localStorage_tmp = window.localStorage;
+    var record_tmp = JSON.parse(localStorage_tmp.getItem("record"));
+
+    // if no key, create one
+    if (!record_tmp.hasOwnProperty(key_name)) {
+      record_tmp[key_name] = [key];
+    } else {
+      record_tmp[key_name].push(key);
     }
+
+    // save json
+    var json = JSON.stringify(record_tmp);
+    // save to local storage
+    localStorage_tmp.setItem("record", json);
   }
 
   // check if the node is in header
@@ -239,6 +251,77 @@
       } else {
         checkIfHeader(keyword, node.parentNode, step);
       }
+    }
+  }
+
+  // add listeners to all elements
+  function addListeners() {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].onclick = function () {
+        // print href
+        if (this.href) {
+          try {
+            // template for click
+            var click = {
+              title: this.innerText,
+              url: this.href,
+              type: "body",
+              time: (Date.now() / 1000) | 0,
+            };
+            // check if header
+            checkIfHeader("header", this, click);
+            modifyRecords("clicks", click);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      };
+      elements[i].onmouseenter = function () {
+        if (this.tagName === "A") {
+          try {
+            // template for hover
+            var hover = {
+              title: this.innerText,
+              type: "body",
+              time: (Date.now() / 1000) | 0,
+              event: "mouseenter",
+            };
+            if (this.href) {
+              hover.url = this.href;
+            } else {
+              hover.url = "";
+            }
+            // check if header
+            checkIfHeader("header", this, hover);
+            modifyRecords("hovers", hover);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      };
+      elements[i].onmouseleave = function () {
+        if (this.tagName === "A") {
+          try {
+            // template for hover
+            var hover = {
+              title: this.innerText,
+              type: "body",
+              time: (Date.now() / 1000) | 0,
+              event: "mouseleave",
+            };
+            if (this.href) {
+              hover.url = this.href;
+            } else {
+              hover.url = "";
+            }
+            // check if header
+            checkIfHeader("header", this, hover);
+            modifyRecords("hovers", hover);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      };
     }
   }
 })();
